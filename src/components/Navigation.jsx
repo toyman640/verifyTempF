9d9f57d3a7dba3cb8e96e394227f3e35225ef9d5
toyman640 <falakoadebayor@gmail.com>
2024-07-02 13:10:59 +0100

    Add Login Form

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navigation = () => {
+  <div>
+    <h2>
+      LOGO
+    </h2>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li>Blog</li>
+    </ul>
+  </div>
+}
+
+export default Navigation;
