12fd9a45e3b65f5ea7747b5559f9c98cd8a7e85a
toyman640 <falakoadebayor@gmail.com>
2024-07-03 13:05:14 +0100

    Get form data

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const moveToLogin = () => {
     navigate('/')
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      firstName,
+      lastName,
+      email,
+      password,
+    }
+  }
+
   return (
     <div className='col-4 offset-4 mt-5 pt-5'>
       <h2 className="text-center mb-5">Sign Using this form</h2>
       <Form>
         <Row>
           <Col>
-            <Form.Control required placeholder="First name" />
+            <Form.Control required value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First name" />
           </Col>
           <Col>
-            <Form.Control required placeholder="Last name" />
+            <Form.Control required value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last name" />
           </Col>
         </Row>
         <Form.Group className="mb-3 mt-3" controlId="formGroupEmail">
-          <Form.Control required type="email" placeholder="Enter email" />
+          <Form.Control required type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
-          <Form.Control required type="password" placeholder="Password" />
+          <Form.Control required type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Register
